525c7d69110ce5e5de7909c1963e792f118659c2
Justin Anderson <justinwarrenanderson@gmail.com>
2019-08-25 13:17:42 -0400

    updating ranks

--- app/routes.py
+++ app/routes.py
@@ -1,22 +1,22 @@
-from app import app
-from manipulations import get_recommendation
+# from app import app
+# from manipulations import get_recommendation
 
-def get_pos_recommendation(position):
-    rec = get_recommendation()
-    rec_pos = rec[position]
-    return rec_pos
+# def get_pos_recommendation(position):
+#     rec = get_recommendation()
+#     rec_pos = rec[position]
+#     return rec_pos
 
-@app.route('/')
-@app.route('/index')
-def index():
-    return '<h1>This is a Fantasy Football App</h1>'
+# @app.route('/')
+# @app.route('/index')
+# def index():
+#     return '<h1>This is a Fantasy Football App</h1>'
 
 
-@app.route('/rec')
-def recommendation():
-    return str(get_recommendation())
+# @app.route('/rec')
+# def recommendation():
+#     return str(get_recommendation())
 
-@app.route('/rec/rb')
-def rec_rb():
-    return str(get_pos_recommendation('RB'))
+# @app.route('/rec/rb')
+# def rec_rb():
+#     return str(get_pos_recommendation('RB'))
 
